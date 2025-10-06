214dce7dc9844006aa2d63d6e73a8fac69a25858
arti <nenadmitt@gmail.com>
2025-10-06 23:09:58 +0200

    self deployment

--- contracts/scripts/deploy_self_registrar.ts
+++ contracts/scripts/deploy_self_registrar.ts
@@ -4,14 +4,6 @@ const SELF_UNIVERSAL_VERIFIER = '0xe57F4773bd9c9d8b6Cd70431117d353298B9f5BF';
 const SELF_SCOPE_SEED = 'celo-test-names';
 const L2_REGISTRY_ADDRESS = "0x968A5c0f00F5D6CE6B29Ee9fD8e4Ea5e748a03BE"
 
-//   constructor(
-//     string memory root_ens_name,
-//     string[] memory gateway_urls,
-//     address[] memory signers,
-//     address name_wrapper,
-//     address owner
-//   )
-
 async function main() {
   const selfVerifier = await viem.deployContract('L2SelfRegistrar', [
     SELF_UNIVERSAL_VERIFIER,
