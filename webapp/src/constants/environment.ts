b2b22613e2e2589666a98913b37cc23c39fbfd3b
artii.eth <85312467+nenadmitt@users.noreply.github.com>
2025-11-12 16:27:11 +0100

    Change indexer url

--- webapp/src/constants/environment.ts
+++ webapp/src/constants/environment.ts
@@ -1,6 +1,6 @@
 // Environment configuration
 export const ENV = {
-  INDEXER_URL: import.meta.env.VITE_INDEXER_URL || 'https://celo-indexer.namespace.ninja',
+  INDEXER_URL: import.meta.env.VITE_INDEXER_URL || 'https://celo-indexer-reader.namespace.ninja',
   PARENT_NAME: import.meta.env.PARENT_NAME || "celo.eth",
   ALCHEMY_TOKEN: import.meta.env.ALCHEMY_TOKEN || "R1Z1EUyNCo6VGghfuACAC",
   MAX_SELF_CLAIM_COUNT: import.meta.env.MAX_SELF_CLAIM_COUNT ? Number(import.meta.env.MAX_SELF_CLAIM_COUNT) : 1,
